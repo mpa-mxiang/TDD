713cc2cc268d4005868dee59ca0b8cb935f2490c
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-07 17:54:23 -0400

    Create solver.rb

---
+++ solver.rb
@@ -0,0 +1,24 @@
+class Solver
+    def factorial(n)
+      raise ArgumentError, "Input must be a non-negative integer" if n < 0
+  
+      return 1 if n == 0
+  
+      result = 1
+      (1..n).each { |i| result *= i }
+      result
+    end
+  
+    def reverse(word)
+      word.reverse
+    end
+  
+    def fizzbuzz(n)
+      return "fizzbuzz" if n % 15 == 0
+      return "fizz" if n % 3 == 0
+      return "buzz" if n % 5 == 0
+  
+      n.to_s
+    end
+  end
+  
