64111055656b6de06f842ab5a1acfec23c803a18
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-09 13:54:29 -0600

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
+# we create the class solver
 class Solver
-    def factorial(n)
-      raise ArgumentError, "Input must be a non-negative integer" if n < 0
-  
-      return 1 if n == 0
-  
-      result = 1
-      (1..n).each { |i| result *= i }
-      result
-    end
-  
-    def reverse(word)
-      word.reverse
-    end
-  
-    def fizzbuzz(n)
-      return "fizzbuzz" if n % 15 == 0
-      return "fizz" if n % 3 == 0
-      return "buzz" if n % 5 == 0
-  
-      n.to_s
-    end
+  def factorial(num)
+    raise ArgumentError, 'Input must be a non-negative integer' if num.negative?
+
+    return 1 if num.zero?
+
+    result = 1
+    (1..num).each { |i| result *= i }
+    result
   end
-  
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 15).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
+end
--- solver_spec.rb
+++ solver_spec.rb
@@ -1,50 +1,57 @@
+# frozen_string_literal: true
+
 require_relative './solver'
 
 RSpec.describe Solver do
   subject(:solver) { Solver.new }
 
-  describe "#factorial" do
-    it "returns 1 for 0" do
+  describe '#factorial' do
+    it 'returns 1 for 0' do
       expect(solver.factorial(0)).to eq(1)
     end
 
-    it "returns the correct factorial for positive integers" do
+    it 'returns the correct factorial for positive integers' do
       expect(solver.factorial(1)).to eq(1)
       expect(solver.factorial(5)).to eq(120)
       expect(solver.factorial(10)).to eq(3_628_800)
     end
 
-    it "raises an exception for negative integers" do
+    it 'raises an exception for negative integers' do
       expect { solver.factorial(-1) }.to raise_error(ArgumentError)
     end
   end
 
-  describe "#reverse" do
-    it "returns the reversed word" do
-      expect(solver.reverse("hello")).to eq("olleh")
-      expect(solver.reverse("ruby")).to eq("ybur")
+  describe '#reverse' do
+    it 'returns the reversed word' do
+      expect(solver.reverse('hello')).to eq('olleh')
+      expect(solver.reverse('ruby')).to eq('ybur')
     end
   end
+end
 
-  describe "#fizzbuzz" do
+# fizzbuzz
+
+RSpec.describe Solver do
+  subject(:solver) { Solver.new }
+  describe '#fizzbuzz' do
     it "returns 'fizz' for multiples of 3" do
-      expect(solver.fizzbuzz(3)).to eq("fizz")
-      expect(solver.fizzbuzz(9)).to eq("fizz")
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+      expect(solver.fizzbuzz(9)).to eq('fizz')
     end
 
     it "returns 'buzz' for multiples of 5" do
-      expect(solver.fizzbuzz(5)).to eq("buzz")
-      expect(solver.fizzbuzz(25)).to eq("buzz")
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+      expect(solver.fizzbuzz(25)).to eq('buzz')
     end
 
     it "returns 'fizzbuzz' for multiples of both 3 and 5" do
-      expect(solver.fizzbuzz(15)).to eq("fizzbuzz")
-      expect(solver.fizzbuzz(30)).to eq("fizzbuzz")
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
     end
 
-    it "returns the number as a string for non-multiples" do
-      expect(solver.fizzbuzz(2)).to eq("2")
-      expect(solver.fizzbuzz(11)).to eq("11")
+    it 'returns the number as a string for non-multiples' do
+      expect(solver.fizzbuzz(2)).to eq('2')
+      expect(solver.fizzbuzz(11)).to eq('11')
     end
   end
 end
